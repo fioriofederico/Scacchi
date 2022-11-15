948125b0473c4cc6d7cd9c267bf956a82f620bf8
Vincenzo Maria Calandra <vincenzomariacalandra@MacBook-Pro-di-Vincenzo.local>
2022-11-15 12:22:46 +0100

    Now we have Queen

--- Regina.py
+++ Regina.py
@@ -9,14 +9,14 @@ Created on Thu Nov 14 10:10:02 2022
 from Pezzo import Pezzo
 
 
-class Regina(Pezzo):
+class Torre(Pezzo):
     """
     implementa la Regina
     """
 
     def __init__(self, colore, posizione=None):
         super().__init__(colore, posizione, 'Regina')
-        self.graphic_rep = '\u2655' if self.colore == 'W' else '\u265b'
+        self.graphic_rep = '\u265B' if self.colore == 'W' else '\u2655c'
 
     def verifica_mossa(self, destinazione):
         """
@@ -34,17 +34,24 @@ class Regina(Pezzo):
 
         """
         if super().verifica_mossa(destinazione):  # le condizioni generiche sono verificate
-            if self.posizione[0] == destinazione[0]:  # la mossa è lungo la stessa colonna
+
+            # la mossa è lungo la stessa colonna
+            if self.posizione[0] == destinazione[0]:  
                 # verifica che non ci siano pezzi tra la casella di partenza e quella di arrivo
                 # per gestire corretamente il ciclo deve distinguere il caso di mossa per righe crescenti o decrescenti
                 first = self.posizione[1]+1 if self.posizione[1]+1 < destinazione[1] else destinazione[1]+1  # prima riga da esaminare
                 last = destinazione[1] if self.posizione[1]+1 < destinazione[1] else self.posizione[1]  # ultma riga da esaminare
                 for riga in range(first, last):
-                    if not self.scacchiera.get_pezzo([destinazione[0], riga]) == None:  # la casella è occupata
-                        print(f"La mossa non è legale perché è presente un pezzo ({self.scacchiera.get_pezzo([destinazione[0], riga]).nome}) nella casella {destinazione[0]}{riga}")
+                    if not self.scacchiera.get_pezzo([destinazione[0], riga]) == None & self.scacchiera.get_pezzo([destinazione[0], riga]).colore != self.colore:  # la casella è occupata
+                        print(f"La mossa è legale perché è presente un pezzo di colore opposto: ({self.scacchiera.get_pezzo([destinazione[0], riga]).nome}) nella casella {destinazione[0]}{riga}")
+                        return True
+                    elif not self.scacchiera.get_pezzo([destinazione[0], riga]) == None & self.scacchiera.get_pezzo([destinazione[0], riga]).colore == self.colore:
+                        print(f"La mossa non è legale perché è presente un pezzo dello stesso colore: ({self.scacchiera.get_pezzo([destinazione[0], riga]).nome}) nella casella {destinazione[0]}{riga}")
                         return False
                 return True
-            elif self.posizione[1] == destinazione[1]:  # la mossa è lungo la stessa riga
+            
+            # la mossa è lungo la stessa riga
+            elif self.posizione[1] == destinazione[1]:  
                 # verifica che non ci siano pezzi tra la casella di partenza e quella di arrivo
                 # per gestire corretamente il ciclo deve distinguere il caso di mossa per colonne crescenti o decrescenti
                 first = ord(self.posizione[0])+1 if ord(self.posizione[0])+1 < ord(destinazione[0]) else ord(destinazione[0])+1  # prima colonna da esaminare
@@ -55,8 +62,8 @@ class Regina(Pezzo):
                         return False
                 return True
             else:
-                print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per la Regina')
+                print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per la Torre')
                 return False
         else:
-            print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per la Regina')
+            print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per la Torre')
             return False
