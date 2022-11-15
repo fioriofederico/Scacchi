c474a4ee64f5a88965d7dd4a3b3fd6520568d637
Federico Fiorio <federico.fiorio@outlook.it>
2022-11-15 13:33:42 +0100

    Modifica testo risposta pezzo mosse il re file Re.py

--- Re.py
+++ Re.py
@@ -55,8 +55,8 @@ class Re(Pezzo):
                         return False
                 return True
             else:
-                print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per la Re')
+                print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per il Re')
                 return False
         else:
-            print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per la Re')
+            print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per il Re')
             return False
